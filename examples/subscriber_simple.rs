fe9df76a745e33d34bd0890bf4333d98feb11b94
Mathias Kraus <k.hias@gmx.de>
2020-01-20 22:29:01 +0100

    update license header

--- examples/subscriber_simple.rs
+++ examples/subscriber_simple.rs
@@ -1,10 +1,8 @@
-// Copyright 2020 Mathias Kraus. All rights reserved.
+// Copyright 2020 Mathias Kraus - All rights reserved
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Licensed under the Apache License, Version 2.0 <LICENSE or
+// http://www.apache.org/licenses/LICENSE-2.0>. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 use iceoryx_rs::{Runtime, Subscriber, SubscriptionState};
 

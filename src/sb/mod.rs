589660da666d80110adac7ba10a96b056be0331a
Mathias Kraus <k.hias@gmx.de>
2021-04-16 20:31:48 +0200

    use COPYRIGHT file and SPDX license identifier instead of copyright header

--- src/sb/mod.rs
+++ src/sb/mod.rs
@@ -1,8 +1,4 @@
-// Copyright 2020 Mathias Kraus - All rights reserved
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE or
-// http://www.apache.org/licenses/LICENSE-2.0>. This file may not be
-// copied, modified, or distributed except according to those terms.
+// SPDX-License-Identifier: Apache-2.0
 
 mod ffi;
 mod sample;

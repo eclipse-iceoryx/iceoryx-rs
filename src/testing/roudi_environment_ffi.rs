857b102edb934cac8fecf53a6021619a4fa12590
Mathias Kraus <k.hias@gmx.de>
2022-06-20 22:41:58 +0200

    iox-#5 Add RouDiEnvironment for testing

---
+++ src/testing/roudi_environment_ffi.rs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: © Contributors to the iceoryx-rs project
+// SPDX-FileContributor: Mathias Kraus
+
+cpp! {{
+    #include "iceoryx_posh/testing/roudi_environment/roudi_environment.hpp"
+
+    using iox::roudi::RouDiEnvironment;
+}}
+
+cpp_class!(pub unsafe struct RouDiEnvironment as "RouDiEnvironment");
+
+impl RouDiEnvironment {
+    pub(crate) fn new() -> Box<Self> {
+        unsafe {
+            let raw = cpp!([] -> *mut RouDiEnvironment as "RouDiEnvironment*"
+            {
+                return new RouDiEnvironment();
+            });
+
+            Box::from_raw(raw)
+        }
+    }
+}

da8771b6025d6e08ea19723166766cdf30f4c2db
Mathias Kraus <k.hias@gmx.de>
2022-06-21 00:10:57 +0200

    iox-#5 Basic pub sub test

---
+++ src/tests/mod.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: © Contributors to the iceoryx-rs project
+// SPDX-FileContributor: Mathias Kraus
+
+// minimal setup with one publisher and one subscriber exchanging data
+mod basic_pub_sub;

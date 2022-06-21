24aaa3781922972e3dc47231b35af55ec6c86bdc
Mathias Kraus <k.hias@gmx.de>
2022-06-21 23:04:35 +0200

    iox-#23 Refine publisher API

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -6,10 +6,8 @@ mod ffi;
 mod publisher;
 mod publisher_options;
 mod sample;
-mod topic;
 
-pub use publisher::Publisher;
+pub use publisher::{InactivePublisher, Publisher, PublisherBuilder};
 pub use sample::POD;
-pub use topic::{Topic, TopicBuilder};
 
 use publisher_options::PublisherOptions;

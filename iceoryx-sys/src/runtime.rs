60747e3bf613d84c501bd920042362c66a1064a6
Mathias Kraus <k.hias@gmx.de>
2022-06-30 21:19:41 +0200

    iox-#23 Set default log level to WARNING

--- iceoryx-sys/src/runtime.rs
+++ iceoryx-sys/src/runtime.rs
@@ -5,10 +5,13 @@
 use std::ffi::CString;
 
 cpp! {{
+    #include "iceoryx_hoofs/log/logmanager.hpp"
     #include "iceoryx_posh/runtime/posh_runtime.hpp"
 
     using iox::RuntimeName_t;
     using iox::cxx::TruncateToCapacity;
+    using iox::log::LogManager;
+    using iox::log::LogLevelOutput;
     using iox::runtime::PoshRuntime;
 }}
 
@@ -20,6 +23,7 @@ impl Runtime {
         let app_name = app_name.as_ptr();
         unsafe {
             cpp!([app_name as "const char *"] {
+                iox::log::LogManager::GetLogManager().SetDefaultLogLevel(iox::log::LogLevel::kWarn, LogLevelOutput::kHideLogLevel);
                 PoshRuntime::initRuntime(RuntimeName_t(TruncateToCapacity, app_name));
             });
         }

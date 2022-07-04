f35f598646169c38635af5b026e44f05622bd574
Mathias Kraus <k.hias@gmx.de>
2022-07-04 23:29:51 +0200

    iox-#32 Share topic data across examples

--- examples/publisher_simple.rs
+++ examples/publisher_simple.rs
@@ -2,21 +2,15 @@
 // SPDX-FileCopyrightText: © Contributors to the iceoryx-rs project
 // SPDX-FileContributor: Mathias Kraus
 
-use iceoryx_rs::marker::ShmSend;
+mod topic;
+use topic::Counter;
+
 use iceoryx_rs::PublisherBuilder;
 use iceoryx_rs::Runtime;
 
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
-
-#[repr(C)]
-#[derive(Default)]
-struct Counter {
-    counter: u32,
-}
-
-unsafe impl ShmSend for Counter {}
 
 fn main() -> Result<(), Box<dyn Error>> {
     Runtime::init("publisher_simple");

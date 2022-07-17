c35d59b1c7c99625fd2e1e3303f63f019cce4e7a
Mathias Kraus <k.hias@gmx.de>
2022-07-17 19:44:36 +0200

    iox-#36 Add tests for slice publishing and receiving

--- src/tests/basic_pub_sub.rs
+++ src/tests/basic_pub_sub.rs
@@ -9,6 +9,7 @@ use crate::Runtime;
 use crate::SubscriberBuilder;
 
 use anyhow::{anyhow, Result};
+use bytes::{Buf, BufMut};
 
 use std::thread;
 
@@ -95,7 +96,7 @@ fn multi_threaded_subscriber() -> Result<()> {
 }
 
 #[test]
-fn publisher_loaning_but_not_publishing_sample() -> Result<()> {
+fn loan_sample_but_not_publish() -> Result<()> {
     let _roudi = RouDiEnvironment::new();
 
     Runtime::init("basic_pub_sub");
@@ -122,7 +123,7 @@ fn publisher_loaning_but_not_publishing_sample() -> Result<()> {
 }
 
 #[test]
-fn loaning_uninitialized_sample() -> Result<()> {
+fn loan_uninit_sample_and_publish() -> Result<()> {
     let _roudi = RouDiEnvironment::new();
 
     Runtime::init("basic_pub_sub");
@@ -148,6 +149,82 @@ fn loaning_uninitialized_sample() -> Result<()> {
 
     match sample_receiver.take() {
         Some(sample) => assert_eq!(sample.counter, SEND_COUNTER),
+        _ => return Err(anyhow!("Could not read sample")),
+    }
+
+    publisher.stop_offer();
+    subscriber.unsubscribe(sample_receiver);
+
+    Ok(())
+}
+
+#[test]
+fn loan_slice_and_publish() -> Result<()> {
+    let _roudi = RouDiEnvironment::new();
+
+    Runtime::init("basic_pub_sub");
+
+    let (subscriber, sample_receive_token) =
+        SubscriberBuilder::<[u8]>::new("Test", "BasicPubSub", "Counter")
+            .queue_capacity(5)
+            .create()?;
+
+    let publisher = PublisherBuilder::<[u8]>::new("Test", "BasicPubSub", "Counter").create()?;
+
+    const SEND_COUNTER: u32 = 37;
+    let mut sample = publisher
+        .loan_slice_with_alignment(std::mem::size_of::<u32>(), std::mem::align_of::<u32>())?;
+    sample.as_mut().put_u32_le(SEND_COUNTER);
+    publisher.publish(sample);
+
+    let sample_receiver = subscriber.get_sample_receiver(sample_receive_token);
+
+    assert!(sample_receiver.has_data());
+
+    match sample_receiver.take() {
+        Some(sample) => assert_eq!(sample.as_ref().get_u32_le(), SEND_COUNTER),
+        _ => return Err(anyhow!("Could not read sample")),
+    }
+
+    publisher.stop_offer();
+    subscriber.unsubscribe(sample_receiver);
+
+    Ok(())
+}
+
+#[test]
+fn loan_uninit_slice_and_publish() -> Result<()> {
+    let _roudi = RouDiEnvironment::new();
+
+    Runtime::init("basic_pub_sub");
+
+    let (subscriber, sample_receive_token) =
+        SubscriberBuilder::<[Counter]>::new("Test", "BasicPubSub", "Counter")
+            .queue_capacity(5)
+            .create()?;
+
+    let publisher = PublisherBuilder::<[u8]>::new("Test", "BasicPubSub", "Counter").create()?;
+
+    const SEND_COUNTER: u32 = 3773;
+    let mut sample = publisher.loan_uninit_slice_with_alignment(
+        std::mem::size_of::<u32>(),
+        std::mem::align_of::<u32>(),
+    )?;
+    let sample = unsafe {
+        sample.slice_assume_init_mut().put_u32_le(SEND_COUNTER);
+        sample.assume_init()
+    };
+    publisher.publish(sample);
+
+    let sample_receiver = subscriber.get_sample_receiver(sample_receive_token);
+
+    assert!(sample_receiver.has_data());
+
+    match sample_receiver.take() {
+        Some(sample) => {
+            assert_eq!(sample.len(), 1);
+            assert_eq!(sample[0].counter, SEND_COUNTER);
+        }
         _ => return Err(anyhow!("Could not read sample")),
     }
 
